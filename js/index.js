60d8c4bade4bf1bb2d78a0d7fab166486cd41994
ignacix <ignanj8@gmail.com>
2024-05-07 12:34:28 -0300

    Initial commit:  add necessary files to show the page.

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function mensaje(){
+    alert("👨‍💻PASA EL MOUSE POR LA IMAGEN GIGANTE😊  O  😎 PRESIONA EN HOME");
+}
+
+function cambiar(x,ruta){
+    x.src=ruta;
+}
+
+function color(){
+    document.documentElement.style.backgroundColor = 'white';
+}
